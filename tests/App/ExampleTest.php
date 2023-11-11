ab06817c38878fc95903d57ddf5e56d21093c42c
VitaliySuhomlinov <scriptcraft.site@gmail.com>
2023-11-11 16:30:02 +0200

    configured unit tests

---
+++ tests/App/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Test\App;
+
+use App\Example;
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        $example = new Example();
+        $result  = $example->do();
+        self::assertEquals(4, $result);
+    }
+}
